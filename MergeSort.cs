0de110ec5452427c8ac7cf11d4c3e9203b049cf8
sher singh negi <shersingh.negi@landisgyr.com>
2018-02-12 11:50:46 +0530

    Selection Sort

---
+++ MergeSort.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+              List<int> data = new List<int>();
+              Parallel.ForEach(Console.ReadLine().Split(' '), (x) =>
+              {
+
+
+                  data.Add(Convert.ToInt32(x));
+                  
+              }
+              );
+            
+            int [] datatest= MergeSort(data.ToArray(), 0, data.Count-1);
+            foreach (var item in datatest)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        private static int[] MergeSort( int[] dataValues, int start,int end)
+        {
+            if (start<end)
+            {
+                int mid = (start + end) / 2;
+                MergeSort(dataValues, start, mid);
+                MergeSort(dataValues, mid + 1, end);
+                Merge(dataValues, start, mid,end);
+            }
+            return dataValues;
+        }
+
+        private static void Merge( int[] dataValues, int start,int mid, int end)
+        {
+            int[] data2 = new int[end-start+1];
+            int j = start;
+            int k = mid+1;
+
+            for (int i = 0; i < data2.Length; i++)
+            {
+                if (j > mid)
+                {
+                    data2[i] = dataValues[k];
+                    k++;
+                }
+                else if (k > end)
+                {
+                    data2[i] = dataValues[j];
+                    j++;
+                }
+
+                else if (dataValues[j] < dataValues[k])
+                {
+                    data2[i] = dataValues[j];
+                    j++;
+
+                }
+                else
+                {
+                    data2[i] = dataValues[k];
+                    k++;
+                }
+                
+
+               
+                
+            }
+
+            for (int n = start; n < data2.Length; n++)
+            {
+                dataValues[n] = data2[n];
+
+            }
+
+        }
+    }
+}
---
+++ SelectionSort.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class SelectionSort
+    {
+        static void Main(string[] args)
+        {
+            List<int> data = new List<int>();
+
+            Parallel.ForEach(Console.ReadLine().Split(' '), (element) =>
+            {
+
+                data.Add(Convert.ToInt32(element));
+            });
+
+           int[] dataSet= SelectionSortArray(data.ToArray());
+
+            foreach (var item in dataSet)
+            {
+                Console.WriteLine(item);
+            }
+
+
+        }
+
+        private static int[] SelectionSortArray(int[] data)
+        {
+            int min = 0;
+            for (int i = 0; i < data.Length; i++)
+            {
+                min = i;
+
+                for (int j = i; j < data.Length; j++)
+                {
+                    if (data[j] < data[min])
+                    {
+                        min = j;
+                    }
+                }
+
+                int temp = data[min];
+                data[min] = data[i];
+                data[i] = temp;
+            }
+            return data;
+        }
+    }
+}
