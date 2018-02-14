86f82480e9fdc90c36d99d1bf475413cecd6a9ea
sher singh negi <shersingh.negi@landisgyr.com>
2018-02-14 12:33:28 +0530

    Sort

---
+++ Quicksort.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class Quicksort
+    {
+        public static void Main(string[] args)
+        {
+            List<int> data = new List<int>();
+            Parallel.ForEach(Console.ReadLine().Split(' '), (element) =>
+            {
+                data.Add(Convert.ToInt32(element));
+            });
+
+            QuicksortArray(data.ToArray(), 0, data.Count);
+        }
+
+        private static void QuicksortArray(int[] dataElements, int start, int end)
+        {
+            if (start < end)
+            {
+
+
+
+            }
+        }
+    }
+}
